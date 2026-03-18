b356cc941b5ddd9210da51f2ab4c2aa0955b48d3
slowpard <slowpard@slowpard.ru>
2026-03-18 14:22:43 +0000

    MO2 fix

--- runtime_hook_numba.py
+++ runtime_hook_numba.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import multiprocessing
+import shutil
+import tempfile
 
 def get_app_dir():
     if getattr(sys, 'frozen', False):
@@ -45,3 +47,34 @@ class PyInstallerCacheLocator(caching.UserProvidedCacheLocator):
         return self
 
 caching.CacheImpl._locator_classes = [PyInstallerCacheLocator]
+
+
+#MO2 workaround
+_real_stderr = sys.stderr
+sys.stderr = open(os.devnull, "w")
+import customtkinter #surpressing error on init so MO2 people don't complain
+sys.stderr.close()
+sys.stderr = _real_stderr
+
+assets_fonts = os.path.join(get_app_dir(), "_internal", "customtkinter",
+                            "assets", "fonts")
+
+original_load = customtkinter.FontManager.windows_load_font
+
+@classmethod
+def patched_load_font(cls, font_path, *args, **kwargs):
+    if os.path.isfile(font_path):
+        temp_font_dir = os.path.join(tempfile.gettempdir(), "ctk_fonts_cache")
+        os.makedirs(temp_font_dir, exist_ok=True)
+        dest = os.path.join(temp_font_dir, os.path.basename(font_path))
+        shutil.copy2(font_path, dest)
+        print(font_path)
+        result = original_load.__func__(cls, dest, *args, **kwargs)	
+        return result
+    return original_load.__func__(cls, font_path, *args, **kwargs)
+    
+customtkinter.FontManager.windows_load_font = patched_load_font
+customtkinter.FontManager.load_font(os.path.join(assets_fonts, "CustomTkinter_shapes_font.otf"))
+customtkinter.FontManager.load_font(os.path.join(assets_fonts, "Roboto", "Roboto-Regular.ttf"))
+customtkinter.FontManager.load_font(os.path.join(assets_fonts, "Roboto", "Roboto-Medium.ttf"))
+customtkinter.DrawEngine.preferred_drawing_method = "font_shapes"
